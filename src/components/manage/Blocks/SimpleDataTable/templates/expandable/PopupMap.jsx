08e447e2f5cc495d334864693b4ee8cf261c2eba
andreiggr <andrei.ggr@gmail.com>
2022-04-13 15:12:12 +0300

    openlayers markers wip

--- src/components/manage/Blocks/SimpleDataTable/templates/expandable/PopupMap.jsx
+++ src/components/manage/Blocks/SimpleDataTable/templates/expandable/PopupMap.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import { compose } from 'redux';
 import { connectToProviderData } from '@eeacms/volto-datablocks/hocs';
+import { Map } from '@eeacms/volto-openlayers-map/Map';
+import { Layers, Layer } from '@eeacms/volto-openlayers-map/Layers';
+import { openlayers } from '@eeacms/volto-openlayers-map';
 
-import { Map, Marker } from 'pigeon-maps';
+// import { Map, Marker } from 'pigeon-maps';
+
+const getLayerBaseURL = () =>
+  'https://services.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}';
 
 const getProviderDataLength = (provider_data) => {
   return provider_data
@@ -11,9 +17,13 @@ const getProviderDataLength = (provider_data) => {
 };
 
 const PopupMap = ({ rowData, provider_data, mapData }) => {
-  const [mapCenter, setMapCenter] = React.useState([45, 9]);
+  const [mapRendered, setMapRendered] = React.useState(false);
+  const [mapCenter, setMapCenter] = React.useState([9, 45]);
+  const mapRef = React.useRef();
 
   const [selectedData, setSelectedData] = React.useState([]);
+
+  const { proj, source, style, format } = openlayers;
 
   React.useEffect(() => {
     const { long, lat } = mapData;
@@ -30,7 +40,8 @@ const PopupMap = ({ rowData, provider_data, mapData }) => {
     const centerLong = minLong && maxLong ? (minLong + maxLong) / 2 : '';
 
     if (centerLat && centerLong) {
-      setMapCenter([centerLat, centerLong]);
+      setMapCenter([centerLong, centerLat]);
+      centerToPosition({ longitude: centerLong, latitude: centerLat }, 5);
     }
   }, [selectedData, mapData]);
 
@@ -60,20 +71,80 @@ const PopupMap = ({ rowData, provider_data, mapData }) => {
 
   //const uniqueCountries = [...new Set(countries)];
 
+  const centerToPosition = (position, zoom) => {
+    const { proj } = openlayers;
+    return mapRef.current.getView().animate({
+      center: proj.fromLonLat([position.longitude, position.latitude]),
+      duration: 1000,
+      zoom,
+    });
+  };
+
   if (!provider_data) {
     return 'Loading..';
   }
   return (
     <div>
       {selectedData.length > 0 ? (
-        <Map height={400} center={mapCenter} defaultZoom={5}>
+        <React.Fragment>
+          <Map
+            ref={(data) => {
+              mapRef.current = data?.map;
+              if (data?.mapRendered && !mapRendered) {
+                setMapRendered(true);
+              }
+            }}
+            view={{
+              center: proj.fromLonLat(mapCenter),
+              showFullExtent: true,
+              // maxZoom: 1,
+              minZoom: 1,
+              zoom: 5,
+            }}
+            renderer="webgl"
+            // onPointermove={this.onPointermove}
+            // onClick={this.onClick}
+            // onMoveend={this.onMoveend}
+          >
+            <Layers>
+              <Layer.Tile
+                source={
+                  new source.XYZ({
+                    url: getLayerBaseURL(),
+                  })
+                }
+              />
+              <Layer.VectorImage
+                source={
+                  new source.Vector({
+                    loader: function (extent, _, projection) {
+                      const esrijsonFormat = new format.EsriJSON();
+                    },
+                  })
+                }
+                style={
+                  new style.Style({
+                    image: new style.Circle({
+                      radius: 6,
+                      fill: new style.Fill({ color: '#00FF00' }),
+                      stroke: new style.Stroke({ color: '#6A6A6A', width: 1 }),
+                      zIndex: 0,
+                    }),
+                  })
+                }
+                title="1.Sites"
+                zIndex={1}
+              />
+            </Layers>
+          </Map>
+          {/* <Map height={400} center={mapCenter} defaultZoom={5}>
           {selectedData.map((item, i) => {
             const long = item[mapData.long] ? item[mapData.long] : '';
             const lat = item[mapData.lat] ? item[mapData.lat] : '';
-            /* const label = item[mapData.label] ? item[mapData.label] : ''; */
             return <Marker width={30} color={'#00519d'} anchor={[lat, long]} />;
           })}
-        </Map>
+        </Map> */}
+        </React.Fragment>
       ) : (
         <p>No data available for map.</p>
       )}
