423c7c98d75aca0fcbf11993a51101d4f5013c4d
Andrei Grigore <andrei.ggr@gmail.com>
2022-06-22 13:54:50 +0300

    check for blockrenders before using

--- src/components/manage/Blocks/ImageCards/index.js
+++ src/components/manage/Blocks/ImageCards/index.js
@@ -1,14 +1,19 @@
 import Cards from './View';
 
 export default (config) => {
-  config.blocks.blocksConfig.imagecards.blockRenderers = {
-    ...config.blocks.blocksConfig.imagecards.blockRenderers,
-    fise_cards_grid: {
-      title: 'FISE cards grid',
-      view: Cards,
-      schema: Cards.schema,
-      schemaExtender: Cards.schemaExtender,
-    },
-  };
+  if (
+    config.blocks.blocksConfig.imagecards &&
+    config.blocks.blocksConfig.imagecards.blockRenderers
+  ) {
+    config.blocks.blocksConfig.imagecards.blockRenderers = {
+      ...config.blocks.blocksConfig.imagecards.blockRenderers,
+      fise_cards_grid: {
+        title: 'FISE cards grid',
+        view: Cards,
+        schema: Cards.schema,
+        schemaExtender: Cards.schemaExtender,
+      },
+    };
+  }
   return config;
 };
