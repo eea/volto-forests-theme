0815b60f044366138a7dd02541e6c49888ab0eee
Andrei Grigore <andrei.ggr@gmail.com>
2022-06-22 13:18:27 +0300

    config update

--- src/components/manage/Blocks/ImageCards/index.js
+++ src/components/manage/Blocks/ImageCards/index.js
@@ -1,16 +1,13 @@
 import Cards from './View';
 
 export default (config) => {
-  config.blocks.blocksConfig.imagecards = {
-    ...config.blocks.blocksConfig.imagecards,
-    blockRenderers: {
-      ...config.blocks.blocksConfig.imagecards.blockRenderers,
-      fise_cards_grid: {
-        title: 'FISE cards grid',
-        view: Cards,
-        schema: Cards.schema,
-        schemaExtender: Cards.schemaExtender,
-      },
+  config.blocks.blocksConfig.imagecards.blockRenderers = {
+    ...config.blocks.blocksConfig.imagecards.blockRenderers,
+    fise_cards_grid: {
+      title: 'FISE cards grid',
+      view: Cards,
+      schema: Cards.schema,
+      schemaExtender: Cards.schemaExtender,
     },
   };
   return config;
