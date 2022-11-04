eb5bcf97f843a41430cb4996a054db4da5fa9b1d
andreiggr <andrei.ggr@gmail.com>
2022-11-04 16:53:08 +0200

    resolve imports

--- src/constants/Layouts.js
+++ src/constants/Layouts.js
@@ -1,5 +1,11 @@
-import layouts from '@plone/volto/constants/Layouts';
 export default {
-  ...layouts,
+  album_view: 'Album view',
+  event_listing: 'Event listing',
+  full_view: 'All content',
+  listing_view: 'Listing view',
+  summary_view: 'Summary view',
+  tabular_view: 'Tabular view',
+  layout_view: 'Mosaic layout',
+  document_view: 'Document view',
   country_tab_view: 'Country page',
 };
